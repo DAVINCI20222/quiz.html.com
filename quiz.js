778b61dc12c484727c5c50322321713db44fa614
DAVINCI20222 <87623790+DAVINCI20222@users.noreply.github.com>
2021-07-19 01:28:55 +0100

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,457 @@
+//Question bank
+var questionBank= [
+    {
+        question : 'Eritrea, which became the 182nd member of the UN in 1993, is in the continent of',
+        option : ['Asia','Africa','Europe','Australia'],
+        answer : 'Africa'
+    },
+    {
+        question : 'Garampani sanctuary is located at',
+        option : ['Junagarh, Gujarat','Diphu, Assam','Kohima, Nagaland','Gangtok, Sikkim'],
+        answer : 'Diphu, Assam'
+    },
+    {
+        question : 'For which of the following disciplines is Nobel Prize awarded?',
+        option : ['Physics and Chemistry','Physiology or Medicine','Physiology or Medicine','All of the above'],
+        answer : 'All of the above'
+    },
+    {
+        question : 'Hitler party which came into power in 1933 is known as',
+        option : ['Labour Party','Nazi Party','Ku-Klux-Klan','Democratic Party'],
+        answer : 'Nazi Party'
+    },
+    {
+        question : 'Epsom (England) is the place associated with',
+        option : ['Horse racing','Car racing','Shooting','Snooker'],
+        answer : 'Horse racing'
+    },
+    {
+        question : 'Which of these is the biggest country in the world?',
+        option : ['Pakistan','Russia','USA','Algeria'],
+        answer : 'Russia'
+    },
+    {
+        question : 'For what year did Nigeria gain her independence?',
+        option : ['Oct-01-1960','Nov-3-1956','Jan-05-1995','Oct-12-1965'],
+        answer : 'Oct-01-1960'
+    },
+    {
+        question : '____ was the former name for Ghana?',
+        option : ['Siver Bird','Golden Eagle','Gold Coast','Watch Tower'],
+        answer : 'Gold Coast'
+    },
+    {
+        question : '____ is the capital city of USA?',
+        option : ['Newyork','New Jersey','Washington DC','Illionis'],
+        answer : 'Washington DC'
+    },
+    {
+        question : 'What is the capital city of Cameroon?',
+        option : ['Yaounde','Basikal','White Deck','Santa-Cruz'],
+        answer : 'Yaounde'
+    },
+    {
+        question : 'The following countries are all located in south america except?',
+        option : ['Bolivia','Brazil','Chile','Poland'],
+        answer : 'Poland'
+    },
+    {
+        question : '____ is the country with the largest official name in the world?',
+        option : ['Libya','Algeria','UK','Sri Lanka'],
+        answer : 'Libya'
+    },
+    {
+        question : '____ is the longest and biggest river in africa?',
+        option : ['Nipopo','Zambezi','Nile','Niger'],
+        answer : 'Nile'
+    },
+    {
+        question : '____ is the biggest ocean in the world?',
+        option : ['Atlantic Ocean','Pacific Ocean','Indian Ocean','Arctic Ocean'],
+        answer : 'Pacific Ocean'
+    },
+    {
+        question : 'Russia lies in two countinents namely;',
+        option : ['Asia and Africa','South America and Europe','Asia and Europe','Europe and Australia'],
+        answer : 'Asia and Europe'
+    },
+    {
+        question : '____ is the name of the USA presidents aircraft?',
+        option : ['Air Force one','Blue vessel','Aircraft master','Lord of sky'],
+        answer : 'Air Force one'
+    },
+    {
+        question : 'Two of these countries are part of OPEC?',
+        option : ['India and France','Ghana and Isreal','Nigeria and Iraq','Poland and Norway'],
+        answer : 'Nigeria and Iraq'
+    },
+    {
+        question : '____ and ____ are part of Ecowas?',
+        option : ['Nigeria and Ghana','Jamaica and Guyana','South Africa and Lesotho','Egypt and Libya'],
+        answer : 'Nigeria and Ghana'
+    },
+    {
+        question : 'PIN stands for ______________________?',
+        option : ['Personal International Name','Police International Network','Personnal Indentification Number','Pay In Naira'],
+        answer : 'Personnal Indentification Number'
+    },
+    {
+        question : 'DJ stands for ______________?',
+        option : ['Disk Jockey','Death in Jail','Dieing for Justice','Dont jump'],
+        answer : 'Disk Jockey'
+    },
+    {
+        question : 'PC stands for ______________?',
+        option : ['Personnal Call','Public Canoe','Personnal Computer','Please Come'],
+        answer : 'Personnal Computer'
+    },
+    {
+        question : 'ICT stands for _________?',
+        option : ['Information Concerning Turkey','Information Communication Technology','Inter-state communication Transaction','Information Computer Tube'],
+        answer : 'Information Communication Technology'
+    },
+    {
+        question : 'GIG in travel networking in Nigeria stands for _______________?',
+        option : ['God Is Going','God Is Granted','God Is God','God Is Good'],
+        answer : 'God Is Good'
+    },
+    {
+        question : '_____ is the prime minister of UK?',
+        option : ['Jeff Bezos','Benny Hinn','Boris Johnson','Dolly Parton'],
+        answer : 'Boris Johnson'
+    },
+    {
+        question : '____ is the president of Nigeria?',
+        option : ['Muhammed Buhari','Johnny Mathis','Muhammed Bello','Rochas Okorocha'],
+        answer : 'Muhammed Buhari'
+    },
+    {
+        question : '____ is the richest man in Africa?',
+        option : ['Rick Warren','Michael Jordan','Aliko Dangote','Michael Peter'],
+        answer : 'Aliko Dangote'
+    },
+    {
+        question : 'President Muhammed Buhari was born in ____?',
+        option : ['Katsina','Kano','Kaduna','Kebbi'],
+        answer : 'Katsina'
+    },
+    {
+        question : '____ is the worlds rarest gem?',
+        option : ['Diamond','Gold','Painite','Ruby'],
+        answer : 'Painite'
+    },
+    {
+        question : 'Nuclear energy is generated by the breakdown of ____ and ____ atoms?',
+        option : ['Uranium and Copper','Crude oil and Granite','Plutonium and Pearl ','Uranium and Plutonium'],
+        answer : 'Uranium and Plutonium'
+    },
+    {
+        question : '____ and ____ where not mentioned in the bible?',
+        option : ['Cats,Rats','Dog,sheep','Ox,Eagle','Owl,Lion'],
+        answer : 'Cats,Rats'
+    },
+    {
+        question : '____ is the most populated countires in the world?',
+        option : ['China','India','Russia','USA'],
+        answer : 'China'
+    },
+    {
+        question : 'Islam simply means ______________?',
+        option : ['Obey your parents','Submission to the will of God','Submission to your elders','Submission to the will of the Devil'],
+        answer : 'Submission to the will of God'
+    },
+    {
+        question : 'Singapore is a misleading country name in the world that simply means ______?',
+        option : ['Good Winds','Blue vessel','City of Lions','Calm sea'],
+        answer : 'City of Lions'
+    },
+    {
+        question : '____ is a country in West Africa?',
+        option : ['Sudan','Gambia','Morocco','Burundi'],
+        answer : 'Gambia'
+    },
+    {
+        question : '____ is a winter crop in India?',
+        option : ['Wheat','Mangoe','Pear','Yam'],
+        answer : 'Wheat'
+    },
+    {
+        question : 'The first ever powered flight took place in _____?',
+        option : ['1980','1903','1865','1931'],
+        answer : '1903'
+    },
+    {
+        question : '____ bustling grand central terminal station is the worlds largest, with 44 platforms',
+        option : ['Paris','Newyork','Seoul','London'],
+        answer : 'Newyork'
+    },
+    {
+        question : 'How many hills is Rome the capital city of Italy built on?',
+        option : ['12','7','5','9'],
+        answer : '7'
+    },
+    {
+        question : 'Christianity simply means ________?',
+        option : ['Christ is coming','Christ like','Obedience to your society','Cleanliness from sin'],
+        answer : 'Christ like'
+    },
+    {
+        question : 'OPEC was set up in ____?',
+        option : ['1890','1965','1960','1901'],
+        answer : '1960'
+    },
+    {
+        question : '____ and ____ are parts of the Asian Tigers?',
+        option : ['Singapore,Isreal','Taiwan,Korea','Russia,China','Palestian,Pakistan'],
+        answer : 'Taiwan,Korea'
+    },
+    {
+        question : '_______ is the Nickname for Jerusalem?',
+        option : ['Smoke','City of David','City of Light','Big Apple'],
+        answer : 'City of David'
+    },
+    {
+        question : 'If Status of LIberty is to USA and ________ is to China?',
+        option : ['Notre-Dame','Blue vessel','Leaning Tower of Pisa','Great Wall'],
+        answer : 'Great Wall'
+    },
+    {
+        question : '____ is the oldest religion in the world?',
+        option : ['Hindu','Christianity','Judaism','Islam'],
+        answer : 'Hindu'
+    },
+    {
+        question : 'One of these is not a sacred mountain?',
+        option : ['Ararat,','Everest','Olympus','Kilimanjaro'],
+        answer : 'Kilimanjaro'
+    },
+    {
+        question : 'How many disciples did Jesus Christ had?',
+        option : ['12','16','18','14'],
+        answer : '12'
+    },
+    {
+        question : 'All these are the five pillars of Islam except ______?',
+        option : ['Salat','The Hajj','Palen','Zakat',],
+        answer : 'Palen'
+    },
+    {
+        question : 'If Jesus was born in Bethlehem then Muhammed was born in ________?',
+        option : ['Persia','Mecca','Seoul','Texas'],
+        answer : 'Mecca'
+    },
+    {
+        question : 'What is the Roman name for the Zeus God?',
+        option : ['Jupiter','Juno','Apollo','Diana'],
+        answer : 'Jupiter'
+    },
+    {
+        question : 'Diana is a Goddess with a role-Goddess of Hunting-(Beware with what you name your child) then her Greek name stands for ________?',
+        option : ['Artemis','Demeter','Ares','Hera'],
+        answer : 'Artemis'
+    },
+    {
+        question : 'Buddha simply means______?',
+        option : ['The great one','Submission to the will of God','The enlightened one','Lord of sky'],
+        answer : 'The enlightened one'
+    },
+    {
+        question : 'The following where all religious leaders except ______?',
+        option : ['Jesus','Mohammed','Zoroaster','Charlie'],
+        answer : 'Charlie'
+    },
+    {
+        question : 'SINAI is a holy mountain loacted at ____?',
+        option : ['Egypt','Iran','Isreal','Indonesia'],
+        answer : 'Isreal'
+    },
+    {
+        question : '____ is not a christian festival?',
+        option : ['Ramandan','Easter','Lent','Advent'],
+        answer : 'Ramandan'
+    },
+    {
+        question : '____ is not an Islam festiaval?',
+        option : ['Ramandan','Night of Ascent','Guru Nanak','Birthday of Muhammad'],
+        answer : 'Guru Nanak'
+    },
+    {
+        question : 'All of the following where the gifts that Jesus christ received when he was born except?',
+        option : ['Gold','Diamond','Myrrh','Frankincense'],
+        answer : 'Diamond'
+    },
+    {
+        question : 'One of these is a mythical place in the world?',
+        option : ['Avalon','Camry','Honda','Mercedes-Benz'],
+        answer : 'Avalon'
+    },
+    {
+        question : '____ is the safest mode of transport?',
+        option : ['Sea','Road','Air','Rail'],
+        answer : 'Air'
+    },
+    {
+        question : 'Who among these was not a disciple of jesus christ?',
+        option : ['Philip','Peter','Paul','John'],
+        answer : 'Paul'
+    },
+    {
+        question : '____ betrayed our Lord Jesus Christ?',
+        option : ['Batholomew','Andrew','Thomas','Judas Iscariot'],
+        answer : 'Judas Iscariot'
+    },
+    {
+        question : 'The temple of Diana at Ephesus is located at ______ ?',
+        option : ['Turkey','Egypt','Peru','Malaysia'],
+        answer : 'Turkey'
+    },
+    {
+        question : 'One of these was not a war group during the world wars?',
+        option : ['Rangers','Samurai','Vikings','Mongols'],
+        answer : 'Rangers'
+    },
+    {
+        question : 'The Covid began in ______?',
+        option : ['2020','2016','2021','2018'],
+        answer : '2020'
+    },
+    {
+        question : 'One of these was not a great military commander?',
+        option : ['Julius Caesar','Alexander the Great','Horatio Nelson','Lord Peter'],
+        answer : 'Lord Peter'
+    },
+    {
+        question : '____ is the fear of Love?',
+        option : ['Acrophobia','Philophobia','Hemophobia','Aerophobia'],
+        answer : 'Philophobia'
+    },
+    {
+        question : '____ is the fear of water?',
+        option : ['Pathophobia','Hydrophobia','Autophobia','Pedophobia'],
+        answer : 'Hydrophobia'
+    },
+    {
+        question : 'The first writing was invented in ____?',
+        option : ['Sudan','Israel','Iraq','Qatar'],
+        answer : 'Iraq'
+    },
+    {
+        question : 'A lack of raw materials in ________ led to the development of specialized higt-tech industries,bringing the country great wealth?',
+        option : ['New Zealand','Netherlands','Bhutan','Switzerland'],
+        answer : 'Switzerland'
+    },
+    {
+        question : 'The first Plough was called an ___?',
+        option : ['Arc','Ard','Axe','Hoe'],
+        answer : 'Ard'
+    },
+    {
+        question : '____ is the oldest university in the world?',
+        option : ['Caleb University','Parma','Al-Azhari','Quaraouyine'],
+        answer : 'Quaraouyine'
+    },
+    {
+        question : 'The first books were written in ____?',
+        option : ['Pidgin','Latin','Arabic','Ighala'],
+        answer : 'Latin'
+    },
+    {
+        question : 'The first piano was built by ____?',
+        option : ['Bartolomeo Christofore','Adolphe Sax','Amadeus Mozart','Leonrdo da Vinci'],
+        answer : 'Bartolomeo Christofore'
+    },
+    {
+        question : '____ is the name of the USA presidents aircraft?',
+        option : ['Air Force one','Blue vessel','Aircraft master','Lord of sky'],
+        answer : 'Air Force one'
+    },
+    {
+        question : '____ is the name of the USA presidents aircraft?',
+        option : ['Air Force one','Blue vessel','Aircraft master','Lord of sky'],
+        answer : 'Air Force one'
+    },
+    {
+        question : '____ is the name of the USA presidents aircraft?',
+        option : ['Air Force one','Blue vessel','Aircraft master','Lord of sky'],
+        answer : 'Air Force one'
+    },
+    {
+        question : '____ is the name of the USA presidents aircraft?',
+        option : ['Air Force one','Blue vessel','Aircraft master','Lord of sky'],
+        answer : 'Air Force one'
+    },
+    {
+        question : '____ is the name of the USA presidents aircraft?',
+        option : ['Air Force one','Blue vessel','Aircraft master','Lord of sky'],
+        answer : 'Air Force one'
+    },
+    ]
+
+    var question= document.getElementById('question');
+    var quizcontainer= document.getElementById('quiz-container');
+    var scorecard= document.getElementById('scorecard');
+    var option0= document.getElementById('option0');
+    var option1= document.getElementById('option1');
+    var option2= document.getElementById('option2');
+    var option3= document.getElementById('option3');
+    var next= document.querySelector('.next');
+    var points= document.getElementById('score');
+    var span= document.querySelectorAll('span');
+    var i=0;
+    var score= 0;
+
+    function displayQuestion(){
+        for(var a=0;a<span.length;a++){
+            span[a].style.background='none';
+        }
+        question.innerHTML= 'Q.'+(i+1)+' '+questionBank[i].question;
+        option0.innerHTML= questionBank[i].option[0];
+        option1.innerHTML= questionBank[i].option[1];
+        option2.innerHTML= questionBank[i].option[2];
+        option3.innerHTML= questionBank[i].option[3];
+        stat.innerHTML= "Question"+' '+(i+1)+' '+'of'+' '+questionBank.length;
+    }
+function calcScore(e){
+    if(e.innerHTML===questionBank[i].answer & score<questionBank.length)
+    {
+        score= score+1;
+        document.getElementById(e.id).style.background= 'limegreen';
+    }
+    else{
+        document.getElementById(e.id).style.background= 'tomato';
+    }
+    setTimeout(nextQuestion,300);
+}
+
+function nextQuestion(){
+    if(i<questionBank.length-1)
+    {
+        i=i+1
+        displayQuestion();
+    }
+    else{
+        points.innerHTML= score+ '/'+
+        questionBank.length;
+        quizContainer.style.display= 'none'
+        scoreboard.style.display= 'block'
+    }
+}
+next.addEventListener('click',nextQuestion);
+
+function backToQuiz(){
+    location.reload();
+}
+
+function checkAnswer(){
+    var answerBank= document.getElementById('answerBank');
+    var answers= document.getElementById('answers');
+    answerBank.style.display= 'block';
+    scoreboard.style.display= 'block';
+    for(var a=0;a<questionBank.length;a++)
+    {
+        var list= document.createElement('li');
+        list.innerHTML= questionBank[a].answer;
+        answers.appendChild(list);
+    }
+}
+displayQuestion()
